9d7fdb7716da0f6642cb60afaa85ecd2017b1c6c
Boór András <BoorA@nemzetiutdij.hu>
2021-01-02 10:45:31 +0100

    add secrets.h -pattern

---
+++ secrets_pattern.h
@@ -0,0 +1,33 @@
+
+#define TITLE                 "Nappali"
+
+// =============================
+// Pinout configuration
+// =============================
+
+//    Outputs
+#define HEATER1_PIN1                    5
+#define HEATER1_PIN2                    2
+
+
+// =============================
+// Output state configurations
+// =============================
+#define ON_OUTPUT_STATE               LOW
+#define OFF_OUTPUT_STATE             HIGH
+#define FEEDBACK_ON_OUTPUT_STATE     HIGH
+#define FEEDBACK_OFF_OUTPUT_STATE     LOW
+
+
+// =============================
+// WiFi credentials
+// =============================
+
+#define SECRET_SSID                "???"    // replace MySSID with your WiFi network name
+#define SECRET_PASS           "???"    // replace MyPassword with your WiFi password
+
+// =============================
+// ThingSpeak connection details
+// =============================
+#define SECRET_CH_ID                 123456     // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "ABCDEFGHIJKLMNOP"   // replace XYZ with your channel write API Key
