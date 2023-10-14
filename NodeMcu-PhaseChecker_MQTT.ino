8a6f5d45a1b2238c922ac386cfd55d6e584e2a54
Barbár <bbk.barbar@gmail.com>
2023-10-14 08:49:17 +0200

    Add MQTT funcitonality

--- NodeMcu-PhaseChecker_MQTT.ino
+++ NodeMcu-PhaseChecker_MQTT.ino
@@ -1,11 +1,19 @@
 /* 
  * IoT thermostat 1ch with new ThingSpeak API
- * Version 0.1 SNAPSHOT
+ * Version 1.2 RC
 
  * Version history:
  *    v0.1 add basic update for ThingSpeak
  *         add working local website
+ *         
+ *    v1.2 add this device as MQTT client
+ *         
  */
+
+/*
+ * Used board setting:
+ * NodeMCU 1.0 (ESP-12E Module)
+ */ 
 
 //#define MOCK_TEMPERATURE_SENSORS
 
@@ -15,12 +23,21 @@
 #include <ESP8266WebServer.h>
 #include <WiFiClient.h>
 
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
+
 #include <ThingSpeak.h>
 #include "secrets.h"
 
-#define VERSION "v1.1 RC"
+#define VERSION "v1.2 SNAPSHOT"
+#define IOT_DEVICE_ID  1
 
-#define SOFTWARE_NAME "Phase checker"
+#define SOFTWARE_NAME "Phase checker MQTT"
+
+#define TITLE SOFTWARE_NAME
+
+// Temperature MQTT Topic(s)
+#define MQTT_PUB_PHASE_STATE "boorfeszek/energy-cheap/state"
 
 
 //==================================
@@ -29,7 +46,7 @@
 // Pin configuration defined in "secrets.h"
 
 //==================================
-// CONSTANTS FOR CALCULATIONS
+//
 //==================================
 
 #define SERVER_PORT 81
@@ -41,8 +58,21 @@
 //==================================
 // WIFI
 //==================================
-const char* ssid = SECRET_SSID;
-const char* password = SECRET_PASS;
+const char* ssid = S_SSID;
+const char* password = S_PASS;
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+//==================================
+// MQTT broker
+//==================================
+#define MQTT_HOST "192.168.1.190"
+#define MQTT_PORT 1883
 
 
 //==================================
@@ -165,29 +195,58 @@ void HandleNotFound(){
   server.send(404, "text/html", message);
 }
 
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  Serial.println("MQTT server set to: " + String(MQTT_HOST));
+  WiFi.begin(ssid, password);
+}
 
 
 
 void setup() {
 
   // Output
-  pinMode(HEATER1_PIN1, OUTPUT);
   pinMode(13, OUTPUT);
   pinMode(D7, OUTPUT);
   pinMode(BUILT_IN_LED, OUTPUT);
   pinMode(analogPin, INPUT);
-  /*pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);/**/
-  #ifdef HEATER1_PIN2
-    pinMode(HEATER1_PIN2, OUTPUT);
-  #endif
-  #ifdef FEEDBACK_1
-    pinMode(FEEDBACK_1, OUTPUT);
-  #endif
   
   // Setup Serial port speed
   Serial.begin(115200);
@@ -198,8 +257,21 @@ void setup() {
 
   digitalWrite(5, HIGH);
 
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  //mqttClient.onSubscribe(onMqttSubscribe);
+  //mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  Serial.println("MQTT server set to: " + String(MQTT_HOST));
+  // If your broker requires authentication (username and password), set them below
+  mqttClient.setCredentials("homeassistant", "Iephi1puhiexeiWiecie4aifooreramo5pooNgeem2ahvie7Thaibae1booxaech");
+
   // Setup WIFI
-  WiFi.begin(ssid, password);
+  connectToWifi();
   Serial.println("");
 
   // Wait for WIFI connection
@@ -210,6 +282,12 @@ void setup() {
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
+
+  // for reconnecting feature
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+  WiFi.hostname(String(SOFTWARE_NAME) + " [" + String(IOT_DEVICE_ID) + "] " + String(VERSION));
+  
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   digitalWrite(A0, LOW);
@@ -221,8 +299,8 @@ void setup() {
   Serial.println("HTTP server started at ip " + WiFi.localIP().toString() );
   
   // ThingSpeak
-  ThingSpeak.begin(client);  // Initialize ThingSpeak
-  Serial.println("TS initialized.");
+  //ThingSpeak.begin(client);  // Initialize ThingSpeak
+  //Serial.println("TS initialized.");
   digitalWrite(BUILT_IN_LED, HIGH);
 
 }
@@ -235,8 +313,6 @@ void loop() {
   
   val = analogRead(analogPin);  // read the input pin
   
-
-    
   if(val > 512){
     if(lastValue != 1){
       digitalWrite(BUILT_IN_LED, LOW);
@@ -244,14 +320,13 @@ void loop() {
       currentState = 1;
       Serial.println("time: " + String(t) + " i: " + String(i) + "\tVal: " + String(val));
       needToUpdate = 1;
+      
+      // Publish MQTT msg
+      uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_PHASE_STATE, 1, true, String(currentState).c_str());                            
+      Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_PHASE_STATE, packetIdPub1);
+      Serial.printf("Message: %i \n", currentState);
     }
-    /*
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);  /**/
+    
     i++;
   }else{
     if(lastValue != 0){
@@ -260,19 +335,19 @@ void loop() {
       currentState = 0;
       Serial.println("time: " + String(t) + " i: " + String(i) + "\tVal: " + String(val));
       needToUpdate = 1;
+      // Publish MQTT msg
+      uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_PHASE_STATE, 1, true, String(currentState).c_str());                            
+      Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_PHASE_STATE, packetIdPub1);
+      Serial.printf("Message: %i \n", currentState);
     }
-    /*digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW); /**/
     i++;
   }
 
+  /*
   if(needToUpdate && canUpdateTS()){
     updateTS(currentState);
     needToUpdate = 0;
   }
+  /**/
   
 }
